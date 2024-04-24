5eec9363b147cd38c2043454b56d66dffee82a3d
Ivasecko48 <robertivasecko2001@gmail.com>
2024-04-24 13:56:09 +0200

    napravljen objekt za procesore s 3 polja nizova

--- js/main.js
+++ js/main.js
@@ -1,10 +1,155 @@
-function showHideOptions() {
-    var processorType = document.getElementById("processor-type").value;
-    var gpuOptions = document.getElementById("gpu-options");
+const processors ={
+    highEndCPU: [ {
+        procName: "Core i9 13900k",
+        id: 1,
+        manufacturer: "Intel",
+        coresThreads: "24/32(8+16)",
+        baseBoostGHz: "3.0/5.8",
+        price: 579, currencySign:"€", currencyCode: "EUR"
+    },
+    {
+        procName: "Core i7 13700k",
+        id: 2,
+        manufacturer: "Intel",
+        coresThreads: "16/24(8+8)",
+        baseBoostGHz: "3.4/5.4",
+        price: 399, currencySign:"€", currencyCode: "EUR"
+    },
+    {
+        procName: "Core i5 13600k",
+        id: 3,
+        manufacturer: "Intel",
+        coresThreads: "14/20(6+8)",
+        baseBoostGHz: "3.5/5.1",
+        price: 299, currencySign:"€", currencyCode: "EUR"
+    },
+    {
+        procName: "Ryzen 9 7950X",
+        id: 4,
+        manufacturer: "AMD",
+        coresThreads: "16/32",
+        baseBoostGHz: "4.5/5.7",
+        price: 569, currencySign:"€", currencyCode: "EUR"
+    },
+    {
+        procName: "Ryzen 7 7700X",
+        id: 5,
+        manufacturer: "AMD",
+        coresThreads: "8/16",
+        baseBoostGHz: "4.5/5.4",
+        price: 349, currencySign:"€", currencyCode: "EUR"
+    },
+    {
+        procName: "Ryzen 5 7600X",
+        id: 6,
+        manufacturer: "AMD",
+        
+        coresThreads: "6/12",
+        baseBoostGHz: "4.7/5.3",
+        price: 249, currencySign:"€", currencyCode: "EUR"
+    },
+],
+midCPU:
+[{
+    procName: "Core i9 13900k",
+    id: 1,
+    manufacturer: "Intel",
+    coresThreads: "24/32(8+16)",
+    baseBoostGHz: "3.0/5.8",
+    price: 579, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Core i7 13700k",
+    id: 2,
+    manufacturer: "Intel",
+    coresThreads: "16/24(8+8)",
+    baseBoostGHz: "3.4/5.4",
+    price: 399, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Core i5 13600k",
+    id: 3,
+    manufacturer: "Intel",
+    coresThreads: "14/20(6+8)",
+    baseBoostGHz: "3.5/5.1",
+    price: 299, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Ryzen 9 7950X",
+    id: 4,
+    manufacturer: "AMD",
+    coresThreads: "16/32",
+    baseBoostGHz: "4.5/5.7",
+    price: 569, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Ryzen 7 7700X",
+    id: 5,
+    manufacturer: "AMD",
+    coresThreads: "8/16",
+    baseBoostGHz: "4.5/5.4",
+    price: 349, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Ryzen 5 7600X",
+    id: 6,
+    manufacturer: "AMD",
+    
+    coresThreads: "6/12",
+    baseBoostGHz: "4.7/5.3",
+    price: 249, currencySign:"€", currencyCode: "EUR"
+},
+],
+lowEndCPU:
+[{
+    procName: "Core i9 13900k",
+    id: 1,
+    manufacturer: "Intel",
+    coresThreads: "24/32(8+16)",
+    baseBoostGHz: "3.0/5.8",
+    price: 579, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Core i7 13700k",
+    id: 2,
+    manufacturer: "Intel",
+    coresThreads: "16/24(8+8)",
+    baseBoostGHz: "3.4/5.4",
+    price: 399, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Core i5 13600k",
+    id: 3,
+    manufacturer: "Intel",
+    coresThreads: "14/20(6+8)",
+    baseBoostGHz: "3.5/5.1",
+    price: 299, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Ryzen 9 7950X",
+    id: 4,
+    manufacturer: "AMD",
+    coresThreads: "16/32",
+    baseBoostGHz: "4.5/5.7",
+    price: 569, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Ryzen 7 7700X",
+    id: 5,
+    manufacturer: "AMD",
+    coresThreads: "8/16",
+    baseBoostGHz: "4.5/5.4",
+    price: 349, currencySign:"€", currencyCode: "EUR"
+},
+{
+    procName: "Ryzen 5 7600X",
+    id: 6,
+    manufacturer: "AMD",
+    
+    coresThreads: "6/12",
+    baseBoostGHz: "4.7/5.3",
+    price: 249, currencySign:"€", currencyCode: "EUR"
+},
+]
 
-    if (processorType === "Intel") {
-        gpuOptions.style.display = "block"; // Prikazi opcije za GPU
-    } else {
-        gpuOptions.style.display = "none"; // Sakrij opcije za GPU
-    }
-}
+};
